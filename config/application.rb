c391d7bbcabd98e6b4fa5fb5f9f106ded2df2714
jose aguilar <jose.aguilar.miranda@gmail.com>
2023-10-07 19:56:50 -0600

    saving advance

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Proton
   class Application < Rails::Application
+
+    config.assets.enabled = true
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
