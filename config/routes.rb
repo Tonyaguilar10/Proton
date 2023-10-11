8f38e326f0807b2785347effc29bfed92b7e024b
jose aguilar <jose.aguilar.miranda@gmail.com>
2023-10-10 20:18:19 -0600

    front end page about 50%

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
-  resources :general_infos, only: [:show]
-  resources :work_experiences, only: [:show]
-  resources :projects, only: [:show]
+  get 'users/index', to: 'users#index', as: :all_players
+  get 'infos/show', to: 'infos#show', as: :about
+  get 'experiences/show', to: 'experiences#show', as: :experience
+  get 'quarks/show', to: 'quarks#show', as: :project
 end
