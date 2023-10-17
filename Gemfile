d948443a4c8e562b4f8616831077f7dad3c95e52
jose aguilar <jose.aguilar.miranda@gmail.com>
2023-10-17 17:20:58 -0600

    Update database configuration for Heroku

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'uglifier', '>= 4.0.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '7.0.0'
 # Use postgresql as the database for Active Record
-gem 'pg', '~> 1.1'
+gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 5.0'
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2023_10_07_224629) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "experiences", force: :cascade do |t|
     t.string "text"
     t.datetime "created_at", precision: 6, null: false
